3bdf68d314ae89debe3cb2b2a807a526687d2f71
PietroCampodonico <pietro.campodonico@outlook.it>
2021-05-26 17:56:58 +0200

    Link in header e footer generati con Vue

--- script.js
+++ script.js
@@ -1,14 +1,65 @@
 new Vue({
     el: "#app",
     data: {
-        navbar: {
-            home: "",
-            Pages: "",
-            Courses: "",
-            Features: "",
-            Blog: "",
-            Shop: "",
-        }
+        navbarLinks: [{
+            text: "Home",
+            link: "#"
+        }, {
+            text: "Pages",
+            link: "#"
+        }, {
+            text: "Courses",
+            link: "#"
+        }, {
+            text: "Features",
+            link: "#"
+        }, {
+            text: "Blog",
+            link: "#"
+        }, {
+            text: "Shop",
+            link: "#"
+        }],
+
+        footerLinks: [{
+
+        }],
+        
+        explore: [{
+            text: "Start Here",
+            link: "#"
+        }, {
+            text: "Blog",
+            link: "#"
+        }, {
+            text: "About us",
+            link: "#"
+        }, {
+            text: "Success story",
+            link: "#"
+        }, {
+            text: "Courses",
+            link: "#"
+        }, {
+            text: "Contact us",
+            link: "#"
+        }],
+
+        information: [{
+            text: "Start Here",
+            link: "#"
+        }, {
+            text: "Blog",
+            link: "#"
+        }, {
+            text: "About us",
+            link: "#"
+        }, {
+            text: "Success story",
+            link: "#"
+        }],
     },
+
+    
 
 })
