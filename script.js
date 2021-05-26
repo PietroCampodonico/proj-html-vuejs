0cd59ed3459d4d42bf9caee022c9998358c38ee2
PietroCampodonico <pietro.campodonico@outlook.it>
2021-05-26 16:33:07 +0200

    section.fifth ok, inizio stesura codice Vue

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 new Vue({
     el: "#app",
+    data: {
+        navbar: {
+            home: "",
+            Pages: "",
+            Courses: "",
+            Features: "",
+            Blog: "",
+            Shop: "",
+        }
+    },
+
 })
